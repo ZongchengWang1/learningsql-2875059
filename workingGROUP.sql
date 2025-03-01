7f0b576881d7db4924afbae2df48ab5428bbb595
ZongchengWang1 <77102785+ZongchengWang1@users.noreply.github.com>
2025-03-01 19:56:26 +0000

    意义不明

--- workingGROUP.sql
+++ workingGROUP.sql
@@ -1,3 +1,4 @@
-SELECT first_name, COUNT(first_name)
+SELECT state_code, quiz_points,COUNT(quiz_points)
 FROM people
-GROUP BY first_name
+GROUP BY quiz_points, state_code
+ORDER BY quiz_points DESC
