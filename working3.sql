531df6fb7cf1e2c1920e223c0342e159c6a811dd
ZongchengWang1 <77102785+ZongchengWang1@users.noreply.github.com>
2025-03-01 01:08:33 +0000

    Adding more criteria to a statement 2

--- working3.sql
+++ working3.sql
@@ -1,4 +1,7 @@
-SELECT first_name,last_name,state_code,shirt_or_hat
+SELECT first_name,last_name,state_code,shirt_or_hat,company
 FROM people 
 WHERE state_code = 'CA'
 AND shirt_or_hat = 'hat'
+AND team IS 'Angry Ants'
+AND (company IS NOT 'Homenick Group'
+OR state_code = 'CO')
